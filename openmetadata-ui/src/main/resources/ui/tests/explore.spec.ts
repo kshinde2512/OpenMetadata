a330b098059d592aa443cb0e134ae459703bb370
kshinde2512 <shindekushal484@gmail.com>
2022-09-20 17:27:01 +0530

    Exploring playwright tool

---
+++ openmetadata-ui/src/main/resources/ui/tests/explore.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.describe('Go to OM homepage', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:8585');
+  });
+
+  // test('Check Headers', async ({ page }) => {
+  //   await expect(page).toHaveURL('http://localhost:8585/my-data');
+  // });
+});
