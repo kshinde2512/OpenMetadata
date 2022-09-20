32ce5656f66dea12780fdd105ab8bf517537775e
kshinde2512 <shindekushal484@gmail.com>
2022-09-20 18:37:30 +0530

    config changes

--- openmetadata-ui/src/main/resources/ui/tests/explore.spec.ts
+++ openmetadata-ui/src/main/resources/ui/tests/explore.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 
 test.describe('Go to OM homepage', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:8585');
+    await page.goto('http://localhost:8585/my-data');
   });
 
   // test('Check Headers', async ({ page }) => {
