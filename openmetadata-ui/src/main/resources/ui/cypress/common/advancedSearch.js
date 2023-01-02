5e7c24b2ef1cfffe1ecf35fae754b31bd2b6d3d0
kshinde2512 <shindekushal484@gmail.com>
2023-01-02 17:45:51 +0530

    failure fix

--- openmetadata-ui/src/main/resources/ui/cypress/common/advancedSearch.js
+++ openmetadata-ui/src/main/resources/ui/cypress/common/advancedSearch.js
@@ -22,6 +22,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import { MYSQL } from '../../constants/service.constants';
 import { interceptURL, verifyResponseStatusCode } from './common';
 
 const dropdown_group_1 =
