02db411bc8e5d620e44ed5d15804730e8dd7d14f
kshinde2512 <shindekushal484@gmail.com>
2023-01-03 21:16:19 +0530

    removing skip from test cases

--- openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/AdvanceSearch.spec.js
+++ openmetadata-ui/src/main/resources/ui/cypress/e2e/Flow/AdvanceSearch.spec.js
@@ -10,18 +10,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-/*
- *  Copyright 2021 Collate
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  http://www.apache.org/licenses/LICENSE-2.0
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 import {
     addOwner,
     addTag,
@@ -53,13 +41,13 @@ describe('Advance search should work properly for all fields', () => {
     cy.get('[data-testid="appbar-item-explore"]').and('be.visible').click();
   });
 
-  it.skip('Pre-requisite for advance search', () => {
+  it('Pre-requisite for advance search', () => {
     addOwner(FIELDS.Owner.searchCriteriaFirstGroup);
     addTier(FIELDS.Tiers.searchCriteriaFirstGroup);
     addTag(FIELDS.Tags.searchCriteriaFirstGroup);
   });
 
-  it.skip('Mysql ingestion', () => {
+  it('Mysql ingestion', () => {
     interceptURL(
       'GET',
       'api/v1/teams/name/Organization?fields=*',
